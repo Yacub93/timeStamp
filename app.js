5afa8d5c62e3c90367e603337fe308972a5094e4
Yacub Ali <Yacub93@Yacubs-MacBook-Pro.local>
2017-05-26 20:46:45 +0100

    Date formatting added to timestamp

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var cors = require('cors');
+var moment = require('moment'); // date formatting
 
 // Create an instance of express for timeStamp app and instantiate bodyParser and cors
 
@@ -13,11 +14,39 @@ app.use(cors());
 // GET request - GET call to return to JSON that formats natural and UNIX date
 app.get('/dateValues/:value', function(req, res, next){
 	// console.log('URL works');
-var dateVal = req.params.value; //get date value parameter from URL
+var dateVal = req.params.value; //get date value from parameter in URL
 
-	res.json({unix : dateVal}); // return JSON response 
+// Date formatting options - natural date
+var dateFormattingOptions = {
 
+	weekday: 'long',
+	day:     'numeric',
+	month:   'long',
+	year:    'numeric'
+	
 
+}; 
+
+	    // Check value is a number
+		if (isNaN(dateVal)) {
+
+			var naturalDate = new Date(dateVal);
+			// From date to moment 
+			// moment.locale('en-GB'); 
+			// var naturalDate = moment(new Date(dateVal)); 
+			// naturalDate.format("l");
+			naturalDate = naturalDate.toLocaleDateString('en-GB', dateFormattingOptions);
+			// naturalDate.toUTCString();
+			
+
+			var unixDate = new Date(dateVal).getTime() / 1000;
+		}
+		else {
+			var unixDate = dateVal;
+			var naturalDate = new Date(dateVal * 1000);
+			naturalDate = naturalDate.toLocaleDateString("en-GB", dateFormattingOptions);
+		}
+			res.json({unix: unixDate, natural: naturalDate}); // return JSON response 
 
 });
 
